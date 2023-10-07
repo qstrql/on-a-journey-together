33f5d900d44523132a71f8397050da85bedf9157
qstrql <mjouot@student.42angouleme.fr>
2023-10-08 01:48:50 +0200

    first commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <div class="font-bold text-red-500">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+        </div>
+    </main>
+</template>
